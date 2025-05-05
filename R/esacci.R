1d048fb50054b618ab3ce4e8a936fa785a1bc362
Michael Sumner <mdsumner@gmail.com>
2025-05-06 07:10:19 +1000

    esacci chlor_a monthly tifs

--- R/esacci.R
+++ R/esacci.R
@@ -9,6 +9,15 @@
 #' @examples
 #' dsn <- esacci_chlor_a()
 esacci_chlor_a <- function(time.resolution = "monthly") {
+  template <- "/vsicurl/https://projects.pawsey.org.au/idea-esacci/www.oceancolour.org/thredds/fileServer/cci/v6.0-release/geographic/monthly/chlor_a/%s/ESACCI-OC-L3S-CHLOR_A-MERGED-1M_MONTHLY_4km_GEO_PML_OCx-%s-fv6.0.tif"
+
+  ##199709
+  dates <- seq(as.Date("1997-09-15"), trunc(Sys.Date(), "months") - 15, by = "1 month")
+  YEARMON <- format(dates, "%Y%m")
+  YEAR <- format(dates, "%Y")
+  tibble::tibble(date = as.POSIXct(dates), source = sprintf(template, YEAR, YEARMON))
+}
+esacci_chlor_a_netcdf <- function(time.resolution = "monthly") {
   template <- "vrt:///vsicurl/https://www.oceancolour.org/thredds/fileServer/cci/v6.0-release/geographic/monthly/chlor_a/%s/ESACCI-OC-L3S-CHLOR_A-MERGED-1M_MONTHLY_4km_GEO_PML_OCx-%s-fv6.0.nc?sd_name=chlor_a&a_srs=EPSG:4326&a_ullr=-180,90,180,-90"
 
   ##199709

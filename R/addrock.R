dfdb4d3411ad9cee8871aab8e46bf671545e4c6a
Michael Sumner <mdsumner@gmail.com>
2025-08-06 12:56:01 +1000

    Create addrock.R

---
+++ R/addrock.R
@@ -0,0 +1,7 @@
+#' Medium resolution vector polygons of Antarctic rock outcrop - VERSION 7.3
+#' 
+#' https://data.bas.ac.uk/full-record.php?id=GB/NERC/BAS/PDC/01409
+#' @export
+addrock <- function() {
+  "/vsizip//vsicurl/https://github.com/AustralianAntarcticDivision/rema.proc/raw/refs/heads/master/01_rock_classification/Medium_resolution_vector_polygons_of_Antarctic_rock_outcrop_-_VERSION_7.3.zip/Medium resolution vector polygons of Antarctic rock outcrop - VERSION 7.3/add_rock_outcrop_medium_res_polygon_v7.3.gpkg"
+"
